9b619e648de9c62d697f4ac934da8e9ec621ccbf
MidNoon <75605062+MidNoon@users.noreply.github.com>
2022-03-08 18:05:09 -0600

    Change

--- Flashcard-Android-App-master/Flashcard-Android-App-master/app/src/main/java/com/example/horac/flashcardapp/ExampleFragment.java
+++ Flashcard-Android-App-master/Flashcard-Android-App-master/app/src/main/java/com/example/horac/flashcardapp/ExampleFragment.java
@@ -1,22 +1,17 @@
 package com.example.horac.flashcardapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExampleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ExampleFragment extends Fragment {
-    FlashcardDatabase flashcardDatabase;
-    List<Flashcard> allFlashcards;
-    int currentCardDisplayedIndex = 0;
     public ExampleFragment() {
         // Required empty public constructor
     }
@@ -25,6 +20,29 @@ public class ExampleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_example, container, false);
+        View x = inflater.inflate(R.layout.fragment_example, container, false);
+        x.findViewById(R.id.MinusButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                ExampleFragment.this.startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.right_in, R.anim.left_out);
+            }
+        });
+
+        x.findViewById(R.id.SaveButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String newQuestion = ((EditText)getView().findViewById(R.id.editText)).getText().toString();
+                String newAnswer = ((EditText)getView().findViewById(R.id.editText2)).getText().toString();
+                Intent data = new Intent();
+                data.putExtra("string1", newQuestion);
+                data.putExtra("string2", newAnswer);
+                getActivity().setResult(AddCardActivity.RESULT_OK, data);
+                getActivity().finish();
+            }
+        });
+        return x;
     }
 }
